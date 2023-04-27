de5c085f2e4d8d83542acfaae2dd37a1d89fd634
Daniel Maldonado <danielmaldonado.edu@gmail.com>
2023-04-27 18:18:52 -0300

    cada pregunta tiene opciones de valoración del 1 al 5

--- src/Survey.jsx
+++ src/Survey.jsx
@@ -1,7 +1,16 @@
 function Survey({ questions }) {
+  const votes = [1, 2, 3, 4, 5];
   return (
     <div className="survey">
       <div>{questions[0].texto}</div>
+      {votes.map((vote) => (
+        <div key={vote}>
+          <label htmlFor={vote}>
+            {vote}
+            <input id={vote} type="radio" role="radio" name="vote" />
+          </label>
+        </div>
+      ))}
     </div>
   );
 }
--- src/Survey.spec.js
+++ src/Survey.spec.js
@@ -22,3 +22,24 @@ test("Mostrar una pregunta a la vez", () => {
     screen.queryByText("¿Cuál es tu opinión sobre el servicio al cliente?")
   ).not.toBeInTheDocument();
 });
+
+test("Cada pregunta tiene opciones de valoración del 1 al 5", () => {
+  const questions = [
+    {
+      id: 1,
+      texto: "¿Qué tan satisfecho estás con el producto?",
+    },
+    {
+      id: 2,
+      texto: "¿Cuál es tu opinión sobre el servicio al cliente?",
+    },
+  ];
+
+  render(<Survey questions={questions} />);
+
+  expect(screen.getByRole("radio", { name: "1" })).not.toBeChecked();
+  expect(screen.getByRole("radio", { name: "2" })).not.toBeChecked();
+  expect(screen.getByRole("radio", { name: "3" })).not.toBeChecked();
+  expect(screen.getByRole("radio", { name: "4" })).not.toBeChecked();
+  expect(screen.getByRole("radio", { name: "5" })).not.toBeChecked();
+});
