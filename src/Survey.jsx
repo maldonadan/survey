7ff57f724a6756af723ed8a57625141022f8c5fe
Daniel Maldonado <danielmaldonado.edu@gmail.com>
2023-04-27 23:12:50 -0300

    al finalizar la encuesta, mostrar un resumen con las respuestas del usuario

--- src/Survey.jsx
+++ src/Survey.jsx
@@ -2,10 +2,20 @@ import { useState } from "react";
 
 const votes = [1, 2, 3, 4, 5];
 
-function Survey({ questions }) {
+function Survey({ questions, renderAnsweredQuestion }) {
   const [currentIndexQuestion, setCurrentIndexQuestion] = useState(0);
+  const [answeredQuestions, setAnsweredQuestions] = useState([]);
   const showNextQuestion = () =>
     setCurrentIndexQuestion(currentIndexQuestion + 1);
+  if (!questions[currentIndexQuestion]) {
+    return (
+      <div>
+        {answeredQuestions.map((answeredQuestions, index) => (
+          <div key={index}>{renderAnsweredQuestion(answeredQuestions)}</div>
+        ))}
+      </div>
+    );
+  }
   return (
     <div className="survey">
       <div>{questions[currentIndexQuestion].texto}</div>
@@ -18,7 +28,13 @@ function Survey({ questions }) {
               type="radio"
               role="radio"
               name="vote"
-              onClick={showNextQuestion}
+              onClick={() => {
+                showNextQuestion();
+                setAnsweredQuestions((answeredQuestions) => [
+                  ...answeredQuestions,
+                  { vote, question: questions[currentIndexQuestion] },
+                ]);
+              }}
             />
           </label>
         </div>
--- src/Survey.spec.js
+++ src/Survey.spec.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, within } from "@testing-library/react";
 import Survey from "./Survey";
 
 test("Mostrar una pregunta a la vez", () => {
@@ -67,4 +67,39 @@ test("Mostrar la siguiente pregunta después de que el usuario haya respondido l
   expect(screen.getByRole("radio", { name: "5" })).not.toBeChecked();
 
   screen.getByText("¿Cuál es tu opinión sobre el servicio al cliente?");
+});
+
+test("Al finalizar la encuesta, mostrar un resumen con las respuestas del usuario", () => {
+  const questions = [
+    {
+      id: 1,
+      texto: "¿Qué tan satisfecho estás con el producto?",
+    },
+    {
+      id: 2,
+      texto: "¿Cuál es tu opinión sobre el servicio al cliente?",
+    },
+  ];
+
+  let actualSurveyResults = null;
+
+  render(
+    <Survey
+      questions={questions}
+      renderAnsweredQuestion={(answeredQuestion) =>
+        `Pregunta: ${answeredQuestion.question.texto} Valoración: ${answeredQuestion.vote}`
+      }
+    />
+  );
+
+  fireEvent.click(screen.getByRole("radio", { name: "3" }));
+  fireEvent.click(screen.getByRole("radio", { name: "5" }));
+
+  screen.getByText(
+    "Pregunta: ¿Qué tan satisfecho estás con el producto? Valoración: 3"
+  );
+
+  screen.getByText(
+    "Pregunta: ¿Cuál es tu opinión sobre el servicio al cliente? Valoración: 5"
+  );
 });
