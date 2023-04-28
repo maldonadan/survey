e3bd2c5dd4e78f837270a7e5e9e181d7991e37ce
Daniel Maldonado <danielmaldonado.edu@gmail.com>
2023-04-27 23:22:14 -0300

    refactor: extraigo componente de valoraciones

--- src/Survey.jsx
+++ src/Survey.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-
-const votes = [1, 2, 3, 4, 5];
+import Votes from "./Votes";
 
 function Survey({ questions, renderAnsweredQuestion }) {
   const [answeredQuestions, setAnsweredQuestions] = useState([]);
   const currentIndexQuestion = answeredQuestions.length;
+  const answerQuestion = (vote) =>
+    setAnsweredQuestions((answeredQuestions) => [
+      ...answeredQuestions,
+      { vote, question: questions[currentIndexQuestion] },
+    ]);
   if (!questions[currentIndexQuestion]) {
     return (
       <div>
@@ -17,25 +21,7 @@ function Survey({ questions, renderAnsweredQuestion }) {
   return (
     <div className="survey">
       <div>{questions[currentIndexQuestion].texto}</div>
-      {votes.map((vote) => (
-        <div key={vote}>
-          <label htmlFor={vote}>
-            {vote}
-            <input
-              id={vote}
-              type="radio"
-              role="radio"
-              name="vote"
-              onClick={() => {
-                setAnsweredQuestions((answeredQuestions) => [
-                  ...answeredQuestions,
-                  { vote, question: questions[currentIndexQuestion] },
-                ]);
-              }}
-            />
-          </label>
-        </div>
-      ))}
+      <Votes onClick={answerQuestion} />
     </div>
   );
 }
